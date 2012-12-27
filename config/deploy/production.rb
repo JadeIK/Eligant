f306913e63b6fa19a5b4b2a72e0f144a1ac34f56
Ivan <ivan@linuxmint.linuxmint-domain>
2012-12-27 11:27:45 +0400

    Fixed capfile Signed-off-by: Ivan <ivan@linuxmint.linuxmint-domain>

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,5 +5,5 @@ set :rails_env, "development"
 set :branch, "master"
 
 role :web, domain
-role :app, domain                                      d
+role :app, domain
 role :db,  domain, :primary => true
