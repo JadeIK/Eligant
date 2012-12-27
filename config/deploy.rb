52b9c9f4e1676ac881d00b53cb318c5abf0be1f3
Ivan <ivan@linuxmint.linuxmint-domain>
2012-12-27 14:11:29 +0400

    Minore change

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,45 +1,40 @@
-gem 'capistrano-ext'
-require 'rvm/capistrano'
-require 'capistrano/ext/multistage'
+require 'rvm/capistrano' # Для работы rvm
+require 'bundler/capistrano' # Для работы bundler. При изменении гемов bundler автоматически обновит все гемы на сервере, чтобы они в точности соответствовали гемам разработчика. 
 
-set :application, "jade-cms_production"
-
-set :scm, :git
-set :repository,  "https://github.com/JadeIK/Eligant.git"
-
-set :stages, %w(production development)
-set :default_stage, "production"
-
+set :application, "eligant_production"
+set :rails_env, "production"
+set :domain, "jade@93.189.40.170" # Это необходимо для деплоя через ssh. Именно ради этого я настоятельно советовал сразу же залить на сервер свой ключ, чтобы не вводить паролей.
+set :deploy_to, "/home/jade/#{application}"
 set :use_sudo, false
-set :keep_releases, 5
-set :no_reboot, true
+set :unicorn_conf, "#{deploy_to}/current/config/unicorn.rb"
+set :unicorn_pid, "#{deploy_to}/shared/pids/unicorn.pid"
 
-set :app_server, :unicorn
-set (:unicorn_conf) {"#{current_path}/config/unicorn.rb"}
-set (:unicorn_pid) {"#{deploy_to}/shared/pids/unicorn.pid"}
+#set :rvm_ruby_string, 'ree' # Это указание на то, какой Ruby интерпретатор мы будем использовать.
 
+set :scm, :git # Используем git. Можно, конечно, использовать что-нибудь другое - svn, например, но общая рекомендация для всех кто не использует git - используйте git. 
+set :repository,  "https://github.com/JadeIK/Eligant.git" # Путь до вашего репозитария. Кстати, забор кода с него происходит уже не от вас, а от сервера, поэтому стоит создать пару rsa ключей на сервере и добавить их в deployment keys в настройках репозитария.
+set :branch, "master" # Ветка из которой будем тянуть код для деплоя.
+set :deploy_via, :remote_cache # Указание на то, что стоит хранить кеш репозитария локально и с каждым деплоем лишь подтягивать произведенные изменения. Очень актуально для больших и тяжелых репозитариев.
+
+role :web, domain
+role :app, domain
+role :db,  domain, :primary => true
+
+before 'deploy:restart', 'deploy:migrate' # интеграция rvm с capistrano настолько хороша, что при выполнении cap deploy:setup установит себя и указанный в rvm_ruby_string руби.
+after 'deploy:update','deploy:cleanup'
+#after 'deploy:update_code','bundle install'
+
+
+# Далее идут правила для перезапуска unicorn. Их стоит просто принять на веру - они работают.
+# В случае с Rails 3 приложениями стоит заменять bundle exec unicorn_rails на bundle exec unicorn
 namespace :deploy do
+  task :restart do
+    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D; fi"
+  end
   task :start do
-    run "cd #{current_path} && unicorn -c #{unicorn_conf} -E #{rails_env} -D"
+    run "cd #{current_path} && bundle exec unicorn -c #{unicorn_conf} -e #{rails_env} -D"
   end
   task :stop do
-    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -9 `cat #{unicorn_pid}`; fi"
-  end
-  task :restart do
-    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -9 `cat #{unicorn_pid}`; fi"
-    run "cd #{current_path} && unicorn -c #{unicorn_conf} -E #{rails_env} -D"
-  end
-  #task :precompile do
-  #  run "cd #{current_path} && bundle exec rake assets:precompile"
-  #end
-end
-
-namespace :bundle do
-  task :install do
-    run "cd #{latest_release} && bundle install"
+    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -QUIT `cat #{unicorn_pid}`; fi"
   end
 end
-
-before 'deploy:restart', 'deploy:migrate'
-after "deploy:update", "deploy:cleanup"
-after "deploy:update_code", "bundle:install"
